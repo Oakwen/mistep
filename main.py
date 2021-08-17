3bb23eefac29aae4c333dab410d251d2818860d4
Oakwen <dai.tianwen@gmail.com>
2021-08-17 12:52:31 +0800

    随机步数调整至20000~24000

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ def main(user, passwd, step):
         return
 
     if step == '':
-        print("已设置为随机步数（18000-21000")
-        step = str(random.randint(18000, 21000))
+        print("已设置为随机步数（20000-24000")
+        step = str(random.randint(20000, 24000))
     login_token = 0
     login_token, userid = login(user, password)
     if login_token == 0:
